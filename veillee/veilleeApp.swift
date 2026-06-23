4ed0a09e5ad001d166bf288326ffd007951fe632
Real Limoges <b.real.limoges@gmail.com>
2026-06-23 08:39:28 -0700

    Initial Commit

---
+++ veillee/veilleeApp.swift
@@ -0,0 +1,17 @@
+//
+//  veilleeApp.swift
+//  veillee
+//
+//  Created by Real on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct veilleeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
